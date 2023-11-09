d94bafe40fb31828d2a6b4e42f20384a9111ad74
김덕용 <yh100112@naver.com>
2023-11-09 12:20:43 +0900

    nodejs db query 비동기 순서 공부

--- nodejs_test/app.js
+++ nodejs_test/app.js
@@ -3,11 +3,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 한다.
+var db = mysql.createConnection({         // 연결 객체 생성
+  host     : '127.0.0.1',
+  user     : 'nodejs',
+  password : '1111',
+  database : 'opentutorials',
+});
+
+db.connect();       // mysql 연결
+
+// db.query('select * from topic', function (error, results, fields) {  // 쿼리 실행
+//   if (error) {
+//     console.log(error);
+//   }
+//   console.log(results);
+// });
+
+db.query('select * from topic', function (error, results, fields) {
+  db.query('select * from test', function (error, results, fields) {
+    console.log('두번째 쿼리');
+  });
+  console.log('첫번째 쿼리');
+});
+
+db.end();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
